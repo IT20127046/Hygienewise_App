0eca51c733427873121d75503e5da8cff99e53fb
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-11-03 11:50:11 +0530

    Configured backend project

---
+++ Backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
+const app = express();
+
+//app middleware
+app.use(bodyparser.json());
+app.use(cors());
+
+// Routes
+const sampleRoutes = require('./routes/sampleRoutes');
+app.use(sampleRoutes);
+
+//Backend server running post number
+const port = process.env.PORT || 5000;
+
+//MongoDB database connection url
+const uri = process.env.MONGO_URI;
+
+//Build database connection
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
+
+// Run backend server
+app.listen(port, () => {
+  console.log(`Server is started in port ${port}`);
+});
