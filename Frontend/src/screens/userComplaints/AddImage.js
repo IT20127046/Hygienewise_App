b43302ada1ab58427f9b78b7dbed6b2d91460264
Loshith Hasinda Samararathna <it20127046@my.sliit.lk>
2022-11-08 10:29:10 +0530

    Update complaint screens

--- Frontend/src/screens/userComplaints/AddImage.js
+++ Frontend/src/screens/userComplaints/AddImage.js
@@ -1,6 +1,8 @@
 import {React, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
 import TextInput from '../../components/form/TextInput';
 import FormBackground from '../../components/form/FormBackground';
 import TextArea from '../../components/form/TextArea';
@@ -15,9 +17,52 @@ export default function AddImage({route}) {
     const [place, setPlace] = useState('');
     const [date, setDate] = useState('');
 
+    const [uri, setUri] = useState('');
+
+    const onSelectImage = () => {
+      const options = {
+        storageOptions: {
+          path: 'images',
+          mediaType: 'photo',
+        },
+        includeBase64: true
+      };
+
+      launchImageLibrary(options, res => {
+        console.log('Response =', res);
+        if(res.didCancel){
+          console.log('Cancel');
+        }
+        else if(res.error){
+          console.log('Error');
+        }
+        else if(res.customButton){
+          console.log('Custom Button');
+        }else{
+          //const source = {uri: 'data:image/jpeg;base64,' + res.base64};
+          setImageName(res.assets.fileName);
+        }
+      })
+    }
+
+    const onPressNext = () => {
+
+      const data = {
+          title: title,
+          description: description,
+          imageName: imageName,
+          place: place,
+          date: date
+      }
+  
+      Navigation.navigate("PreviewComplaint", data)
+    }
+
   return (
     <View>
       <Text>Form</Text>
+
+      <Button title={'Select Image'} onPress={onSelectImage}/>
 
       <FormBackground>
         <TextInput
@@ -36,7 +81,7 @@ export default function AddImage({route}) {
           textContentType="text"
         />
 
-        <SubmitButton mode="contained" color="#6495ed">
+        <SubmitButton mode="contained" color="#6495ed" onPress={onPressNext}>
         Next
       </SubmitButton>
 
