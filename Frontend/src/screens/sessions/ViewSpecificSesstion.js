7ceeda4784b4fa4b0fea357dd56dca63b5c12be1
IT20128036 <87744181+IT20128036@users.noreply.github.com>
2022-11-10 11:46:50 +0530

    bug Fixed

--- Frontend/src/screens/sessions/ViewSpecificSesstion.js
+++ Frontend/src/screens/sessions/ViewSpecificSesstion.js
@@ -38,7 +38,7 @@ export default function ViewSpecificSesstion({route}) {
          <View style={styles.background}>
            <Image
              style={styles.image}
-             source={require('../../assets/images/session1.jpg')}
+             source={require('../../assets/images/s3.png')}
            />
          </View>
        </View>
@@ -126,7 +126,7 @@ const styles = StyleSheet.create({
       background: {
         // this shape is a circle
         // border borderRadius same as width and height
-        borderRadius: 400,
+        // borderRadius: 400,
         width: 535,
         height: 600,
         marginLeft: -100, // reposition the circle inside parent view
